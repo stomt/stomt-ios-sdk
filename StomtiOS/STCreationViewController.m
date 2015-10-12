ff0dc35b210949594f663b0297aa4056612b28f4
H3xept <h3xept@gmail.com>
2015-10-12 17:49:39 +0200

    Fixes

--- StomtiOS/STCreationViewController.m
+++ StomtiOS/STCreationViewController.m
@@ -207,7 +207,7 @@ error:
 - (void)sendStomtWithDict:(NSNotification*)notification//Body:(NSString *)body likeOrWish:(kSTObjectQualifier)likeOrWish targetID:(NSString *)targetID
 {
 	NSDictionary* userInfo = [notification userInfo];
-	STObject* stomtObject = [STObject objectWithTextBody:[userInfo objectForKey:@"body"] likeOrWish:(int)[userInfo objectForKey:@"likeOrWish"] targetID:[userInfo objectForKey:@"targetID"]];
+	STObject* stomtObject = [STObject objectWithTextBody:[userInfo objectForKey:@"body"] likeOrWish:(kSTObjectQualifier)[[userInfo objectForKey:@"likeOrWish"] intValue] targetID:[userInfo objectForKey:@"targetID"]];
 	StomtRequest* request = [StomtRequest stomtCreationRequestWithStomtObject:stomtObject];
 	
 	[request sendStomtInBackgroundWithBlock:self.completionBlock];
