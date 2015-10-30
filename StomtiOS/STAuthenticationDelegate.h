bce2da6de4ac6a8b2649ac0607796bb0ba490ebf
H3xept <h3xept@gmail.com>
2015-10-30 15:24:20 +0100

    Preparing for branch iOS 9

---
+++ StomtiOS/STAuthenticationDelegate.h
@@ -0,0 +1,13 @@
+//
+//  STAuthenticationDelegate.h
+//  StomtiOS
+//
+//  Created by Leonardo Cascianelli on 28/10/15.
+//  Copyright © 2015 Leonardo Cascianelli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STAuthenticationDelegate <NSObject>
+
+@end
