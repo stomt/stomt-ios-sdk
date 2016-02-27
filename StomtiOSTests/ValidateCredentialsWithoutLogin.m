892c90bf9a38a7ad04866aff050bf48b39303b9f
H3xept <h3xept@gmail.com>
2016-02-27 16:07:48 +0100

    [*] Various improvements. [+] Availability check request

---
+++ StomtiOSTests/ValidateCredentialsWithoutLogin.m
@@ -0,0 +1,39 @@
+//
+//  ValidateCredentialsWithoutLogin.m
+//  StomtiOS
+//
+//  Created by Leonardo Cascianelli on 27/02/16.
+//  Copyright © 2016 Leonardo Cascianelli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ValidateCredentialsWithoutLogin : StomtTestCase
+
+@end
+
+@implementation ValidateCredentialsWithoutLogin
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
